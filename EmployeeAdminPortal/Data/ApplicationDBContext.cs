1392dac5803dc9e2fc6957d9cbb1642953f02060
nilamkhedekar05 <90793313+nilamkhedekar05@users.noreply.github.com>
2024-06-18 15:31:04 +0530

    Add files via upload

---
+++ EmployeeAdminPortal/Data/ApplicationDBContext.cs
@@ -0,0 +1,19 @@
+﻿
+using EmployeeAdminPortal.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeAdminPortal.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        //public ApplicationDBContext(DBContextOptions<ApplicationDBContext> options) : (base options)
+        //{
+        //}
+        public ApplicationDBContext(DbContextOptions options) : base(options)
+        { 
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
